8cbf6a06ee38b151a5c4a6cd58b99ca1d9f4a322
Leo Ma <leoma@credosemi.com>
2014-06-30 19:11:08 +0800

    Community string check
    Signed-off-by: Leo Ma <leoma@credosemi.com>

--- tests/test.sh
+++ tests/test.sh
@@ -26,11 +26,11 @@ snmpset -v2c -cpublic localhost .1.3.6.1.2.1.4.1.0 s "This agent is really smart
 snmpset -v2c -cpublic localhost .1.3.6.1.2.1.4.1.0 i 8888
 
 #Error test (unaccessible)
-snmpset -v2c -cprivateprivate localhost .1.3.6.1.2.1.1.9.1.1 i 1
+snmpset -v2c -cprivate localhost .1.3.6.1.2.1.1.9.1.1 i 1
 # Error test (wrong type)
-snmpset -v2c -cprivate localhost .1.3.6.1.2.1.4.1.0 s "This agent is really smart!"
+snmpset -v2c -cipprivate localhost .1.3.6.1.2.1.4.1.0 s "This agent is really smart!"
 # OK test
-snmpset -v2c -cprivate localhost .1.3.6.1.2.1.4.1.0 i 8888
+snmpset -v2c -cipprivate localhost .1.3.6.1.2.1.4.1.0 i 8888
 
 snmpwalk -v2c -cpublic localhost .1.3.6.1.2.1.1
 snmpwalk -v2c -cpublic localhost .1.3.6.1
