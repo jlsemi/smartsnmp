2cee869def915cb8a05233bf330a28e53ad49017
leo-ma <begeekmyfriend@gmail.com>
2014-11-19 16:11:45 +0800

    Update test sample for AgentX
    Signed-off-by: leo-ma <begeekmyfriend@gmail.com>

--- tests/agentx_test.sh
+++ tests/agentx_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# SNMPv2c
+
 snmpget -v2c -cpublic localhost .0 
 snmpget -v2c -cpublic localhost .1.3
 snmpget -v2c -cpublic localhost .1.4
@@ -19,13 +21,6 @@ snmpgetnext -v2c -cpublic localhost .1.5.6.7.8.100
 snmpbulkget -v2c -cpublic localhost .1.3.6.1.3.1.1.9
 snmpbulkget -v2c -cpublic localhost .1.3.6.1.3.1.1.9.1.4.2
 
-# Error test (community authorization)
-#snmpset -v2c -cpublic localhost .1.3.6.1.2.1.1.9.1.1 i 1
-# Error test (community authorization)
-#snmpset -v2c -cpublic localhost .1.3.6.1.2.1.4.1.0 s "This agent is really smart!"
-# Error test (community authorization)
-#snmpset -v2c -cpublic localhost .1.3.6.1.2.1.4.1.0 i 8888
-
 # Error test (unaccessible)
 snmpset -v2c -cprivate localhost .1.3.6.1.3.1.1.9.1.1 i 1
 # Error test (wrong type)
@@ -35,3 +30,34 @@ snmpset -v2c -cprivate localhost .1.3.6.1.3.1.4.1.0 i 8888
 
 snmpwalk -v2c -cpublic localhost .1.3.6.1.3
 snmpwalk -v2c -cpublic localhost .1.3.6.1
+
+# SNMPv3
+
+snmpget -u noAuthUser -l noAuthNoPriv localhost .0 
+snmpget -u noAuthUser -l noAuthNoPriv localhost .1.3
+snmpget -u noAuthUser -l noAuthNoPriv localhost .1.4
+snmpget -u noAuthUser -l noAuthNoPriv localhost .1.5.6.7.8.100
+
+snmpget -u noAuthUser -l noAuthNoPriv localhost .1.3.6.1.3.1.1.9.1.2.1
+snmpget -u noAuthUser -l noAuthNoPriv localhost .1.3.6.1.3.1.1.1.0 .1.3.6.1.3.1.1.2.0
+snmpget -u noAuthUser -l noAuthNoPriv localhost .1.3.6.1.3.1.1.9.1.1
+snmpget -u noAuthUser -l noAuthNoPriv localhost .1.3.6.1.3.1.1.9.1.2
+snmpget -u noAuthUser -l noAuthNoPriv localhost .1.3.6.1.3.1.1.9.1.5
+
+snmpgetnext -u noAuthUser -l noAuthNoPriv localhost .0 
+snmpgetnext -u noAuthUser -l noAuthNoPriv localhost .1.3
+snmpgetnext -u noAuthUser -l noAuthNoPriv localhost .1.4
+snmpgetnext -u noAuthUser -l noAuthNoPriv localhost .1.5.6.7.8.100
+
+snmpbulkget -u noAuthUser -l noAuthNoPriv localhost .1.3.6.1.3.1.1.9
+snmpbulkget -u noAuthUser -l noAuthNoPriv localhost .1.3.6.1.3.1.1.9.1.4.2
+
+# Error test (unaccessible)
+snmpset -v3 -unoAuthUser -l noAuthNoPriv localhost .1.3.6.1.3.1.1.9.1.1 i 1
+# Error test (wrong type)
+snmpset -v3 -unoAuthUser -l noAuthNoPriv localhost .1.3.6.1.3.1.4.1.0 s "This agent is really smart!"
+# OK test
+snmpset -v3 -unoAuthUser -l noAuthNoPriv localhost .1.3.6.1.3.1.4.1.0 i 8888
+
+snmpwalk -u noAuthUser -l noAuthNoPriv localhost .1.3.6.1.3
+snmpwalk -u noAuthUser -l noAuthNoPriv localhost .1.3.6.1
