8f3ab485cf7bee19cbe6541b65dcb9e947640cec
leo-ma <begeekmyfriend@gmail.com>
2015-01-04 10:15:29 +0800

    BER interger and unsigned interger data type codec
    Signed-off-by: leo-ma <begeekmyfriend@gmail.com>

--- core/snmp_encoder.c
+++ core/snmp_encoder.c
@@ -41,40 +41,38 @@ ber_int_enc_try(int value)
   len = 0;
 
 #ifdef LITTLE_ENDIAN
-  /* Number zero counts one */
-  i = 1;
-
-  if (value >= 0) {
-    while (i < sizeof(int) && a.buf[i]) {
-      i++;
-    }
-    if (a.buf[i - 1] & 0x80) {
-      i++;
-    }
-  } else {
-    while (i < sizeof(int) && a.buf[i] != 0xff) {
-      i++;
-    }
-    if (!(a.buf[i - 1] & 0x80)) {
-      i++;
-    }
-  }
-
-  len = i;
-#else
-  /* Number zero counts one */
   i = sizeof(int) - 1;
 
   if (value >= 0) {
-    while (i > 0 && a.buf[i - 1]) {
+    while (i > 0 && !a.buf[i]) {
       i--;
     }
     if (a.buf[i] & 0x80) {
       len += 1;
     }
   } else {
-    while (i > 0 && a.buf[i - 1] != 0xff) {
+    while (i > 0 && a.buf[i] == 0xff) {
       i--;
+    }
+    if (!(a.buf[i] & 0x80)) {
+      len += 1;
+    }
+  }
+
+  len += i + 1;
+#else
+  i = 0;
+
+  if (value >= 0) {
+    while (i < sizeof(int) && !a.buf[i]) {
+      i++;
+    }
+    if (a.buf[i] & 0x80) {
+      len += 1;
+    }
+  } else {
+    while (i < sizeof(int) && a.buf[i] == 0xff) {
+      i++;
     }
     if (!(a.buf[i] & 0x80)) {
       len += 1;
@@ -86,6 +84,50 @@ ber_int_enc_try(int value)
 
   return len;
 }
+
+/* Input:  unsigned integer value
+ * Output: none
+ * Return: byte length.
+ */
+static uint32_t
+ber_uint_enc_try(unsigned int value)
+{
+  uint32_t i, len;
+  union anonymous {
+    uint8_t buf[sizeof(unsigned int)];
+    unsigned int tmp;
+  } a;
+
+  a.tmp = value;
+  len = 0;
+
+#ifdef LITTLE_ENDIAN
+  i = sizeof(unsigned int) - 1;
+
+  while (i > 0 && !a.buf[i]) {
+    i--;
+  }
+  if (a.buf[i] & 0x80) {
+    len += 1;
+  }
+
+  len += i + 1;
+#else
+  i = 0;
+
+  while (i < sizeof(unsigned int) && !a.buf[i]) {
+    i++;
+  }
+  if (a.buf[i] & 0x80) {
+    len += 1;
+  }
+
+  len += sizeof(unsigned int) - i;
+#endif
+
+  return len;
+}
+
 
 /* Input:  oid pointer, number of elements
  * Output: none
@@ -126,14 +168,18 @@ ber_value_enc_try(const void *value, uint32_t len, uint8_t type)
   uint32_t ret;
   const oid_t *oid;
   const int *inter;
+  const unsigned int *uinter;
 
   switch (type) {
     case ASN1_TAG_INT:
     case ASN1_TAG_CNT:
-    case ASN1_TAG_GAU:
-    case ASN1_TAG_TIMETICKS:
       inter = (const int *)value;
       ret = ber_int_enc_try(*inter);
+      break;
+    case ASN1_TAG_GAU:
+    case ASN1_TAG_TIMETICKS:
+      uinter = (const unsigned int *)value;
+      ret = ber_uint_enc_try(*uinter);
       break;
     case ASN1_TAG_OBJID:
       oid = (const oid_t *)value;
@@ -141,7 +187,6 @@ ber_value_enc_try(const void *value, uint32_t len, uint8_t type)
       break;
     case ASN1_TAG_OCTSTR:
     case ASN1_TAG_IPADDR:
-    case ASN1_TAG_OPAQ:
       ret = len;
       break;
     case ASN1_TAG_SEQ:
@@ -161,7 +206,7 @@ ber_value_enc_try(const void *value, uint32_t len, uint8_t type)
 static uint32_t
 ber_int_enc(int value, uint8_t *buf)
 {
-  uint32_t i, j;
+  int i, j;
   union anonymous {
     uint8_t buf[sizeof(int)];
     int tmp;
@@ -171,42 +216,41 @@ ber_int_enc(int value, uint8_t *buf)
   j = 0;
 
 #ifdef LITTLE_ENDIAN
-  /* Number zero counts one */
-  i = 1;
-
-  if (value >= 0) {
-    while (i < sizeof(int) && a.buf[i]) {
-      i++;
-    }
-    if (a.buf[i - 1] & 0x80) {
-      buf[j++] = 0x0;
-    }
-  } else {
-    while (i < sizeof(int) && a.buf[i] != 0xff) {
-      i++;
-    }
-    if (!(a.buf[i - 1] & 0x80)) {
-      buf[j++] = 0xff;
-    }
-  }
-
-  while (i > 0) {
-    buf[j++] = a.buf[--i];
-  }
-#else
-  /* Number zero counts one */
   i = sizeof(int) - 1;
 
   if (value >= 0) {
-    while (i > 0 && a.buf[i - 1]) {
+    while (i > 0 && !a.buf[i]) {
       i--;
     }
     if (a.buf[i] & 0x80) {
       buf[j++] = 0x0;
     }
   } else {
-    while (i > 0 && a.buf[i - 1] != 0xff) {
+    while (i > 0 && a.buf[i] == 0xff) {
       i--;
+    }
+    if (!(a.buf[i] & 0x80)) {
+      buf[j++] = 0xff;
+    }
+  }
+
+  while (i >= 0) {
+    buf[j++] = a.buf[i--];
+  }
+#else
+  /* Number zero counts one */
+  i = 0;
+
+  if (value >= 0) {
+    while (i < sizeof(int) && !a.buf[i]) {
+      i++;
+    }
+    if (a.buf[i] & 0x80) {
+      buf[j++] = 0x0;
+    }
+  } else {
+    while (i < sizeof(int) && a.buf[i] == 0xff) {
+      i++;
     }
     if (!(a.buf[i] & 0x80)) {
       buf[j++] = 0xff;
@@ -220,6 +264,55 @@ ber_int_enc(int value, uint8_t *buf)
 
   return j;
 }
+
+/* Input:  unsigned integer value
+ * Output: buffer
+ * Return: byte length.
+ */
+static uint32_t
+ber_uint_enc(int value, uint8_t *buf)
+{
+  int i, j;
+  union anonymous {
+    uint8_t buf[sizeof(unsigned int)];
+    unsigned int tmp;
+  } a;
+
+  a.tmp = value;
+  j = 0;
+
+#ifdef LITTLE_ENDIAN
+  i = sizeof(unsigned int) - 1;
+
+  while (i > 0 && !a.buf[i]) {
+    i--;
+  }
+  if (a.buf[i] & 0x80) {
+    buf[j++] = 0x0;
+  }
+
+  while (i >= 0) {
+    buf[j++] = a.buf[i--];
+  }
+#else
+  /* Number zero counts one */
+  i = 0;
+
+  while (i < sizeof(unsigned int) && !a.buf[i]) {
+    i++;
+  }
+  if (a.buf[i] & 0x80) {
+    buf[j++] = 0x0;
+  }
+
+  while (i < sizeof(unsigned int)) {
+    buf[j++] = a.buf[i++];
+  }
+#endif
+
+  return j;
+}
+
 
 /* Input:  oid pointer, number of elements
  * Output: buffer
@@ -267,14 +360,18 @@ ber_value_enc(const void *value, uint32_t len, uint8_t type, uint8_t *buf)
   const uint8_t *str;
   const oid_t *oid;
   const int *inter;
+  const unsigned int *uinter;
 
   switch (type) {
     case ASN1_TAG_INT:
     case ASN1_TAG_CNT:
-    case ASN1_TAG_GAU:
-    case ASN1_TAG_TIMETICKS:
       inter = (const int *)value;
       ret = ber_int_enc(*inter, buf);
+      break;
+    case ASN1_TAG_GAU:
+    case ASN1_TAG_TIMETICKS:
+      uinter = (const unsigned int *)value;
+      ret = ber_uint_enc(*uinter, buf);
       break;
     case ASN1_TAG_OBJID:
       oid = (const oid_t *)value;
@@ -282,7 +379,6 @@ ber_value_enc(const void *value, uint32_t len, uint8_t type, uint8_t *buf)
       break;
     case ASN1_TAG_OCTSTR:
     case ASN1_TAG_IPADDR:
-    case ASN1_TAG_OPAQ:
       str = (const uint8_t *)value;
       memcpy(buf, str, len);
       ret = len;
