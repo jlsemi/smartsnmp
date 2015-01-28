917e3c8ce3541f70b8b4b113f9540e981c12bf0e
Xiongfei Guo <xfguo@credosemi.com>
2015-01-28 18:06:42 +0800

    Modify manual scripts to support lua coverage test.
    Signed-off-by: Xiongfei Guo <xfguo@credosemi.com>

--- tests/agentx_daemon.sh
+++ tests/agentx_daemon.sh
@@ -1,1 +1,5 @@
-LUA_PATH="lualib/?/init.lua;lualib/?.lua;./?.lua" LUA_CPATH="build/?.so" ./bin/smartsnmpd -c config/agentx.conf
+if [ -n "$LUACOV" ]; then
+	export SYS_LUA_PATH=`lua -e "print(package.path)"`;
+	export LUACOV_OPT=-lluacov
+fi
+LUA_PATH="lualib/?/init.lua;lualib/?.lua;./?.lua;$SYS_LUA_PATH" LUA_CPATH="build/?.so" lua $LUACOV_OPT ./bin/smartsnmpd -c config/agentx.conf
